2f98aab67d7e588fc8fed6b602061c24683286f5
Angie Carrillo <ancarrilloc@gmail.com>
2026-08-02 20:46:21 -0500

    Update API endpoint in getBotReplay function

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ function showTyping() {
 }
 
 async function getBotReplay(userMessage) {
-    const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${apiKey}`;
+const url = `https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent?key=${apiKey}`;
 
     try {
         const response = await fetch(url, {
